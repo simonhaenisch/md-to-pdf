1cbecc58142dac76fdf1b2eff3919c1949947c33
simonhaenisch <simonhaenisch@users.noreply.github.com>
2018-05-08 18:32:14 +1200

    rename a file

--- util/write-pdf.js
+++ util/write-pdf.js
@@ -1,7 +1,7 @@
 const { sep } = require('path');
 const html2pdf = require('html-pdf');
 const getPdfFilePath = require('./get-pdf-file-path');
-const getAssetBasePath = require('./get-asset-base-path');
+const getAssetsBasePath = require('./get-assets-base-path');
 
 /**
  * Create PDF and write it to disk. Returns a promise that resolves once the
@@ -14,9 +14,9 @@ const getAssetBasePath = require('./get-asset-base-path');
  */
 module.exports = (mdFilePath, outputPath, html, config) => {
 	const pdfFilePath = outputPath || getPdfFilePath(mdFilePath);
-	const assetBasePath = getAssetBasePath(mdFilePath);
+	const assetsBasePath = getAssetsBasePath(mdFilePath);
 
-	const options = { base: `file://${assetBasePath}${sep}`, ...config.html_pdf_options };
+	const options = { base: `file://${assetsBasePath}${sep}`, ...config.html_pdf_options };
 
 	return new Promise((resolve, reject) => {
 		html2pdf.create(html, options).toFile(pdfFilePath, (err, res) => {
