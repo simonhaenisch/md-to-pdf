cf55456fc4b0335bc69574f34e470b8144b4df60
simonhaenisch <simonhaenisch@users.noreply.github.com>
2018-05-21 00:44:22 +1200

    read-file: default to utf-8 encoding

--- util/read-file.js
+++ util/read-file.js
@@ -7,5 +7,5 @@ const iconv = require('iconv-lite');
  * @param {String} encoding File encoding
  * @returns its contents as a string.
  */
-module.exports = (file, encoding) =>
+module.exports = (file, encoding = 'utf-8') =>
 	/utf-?8/i.test(encoding) ? fs.readFileSync(file, encoding) : iconv.decode(fs.readFileSync(file), encoding);
