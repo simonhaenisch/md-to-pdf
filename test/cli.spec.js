e6b0ca1c0866f4faeda8e3f661c7c90819f133a2
simonhaenisch <simonhaenisch@users.noreply.github.com>
2019-05-11 13:46:47 +0200

    test: split execution of lib, api and cli specs

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -4,10 +4,7 @@ const exec = promisify(require('child_process').exec);
 const test = require('ava');
 
 test('should compile the basic example to pdf', async t => {
-	const { stdout, stderr } = await exec(
-		`node ${resolve(__dirname, '..', 'cli.js')} ${resolve(__dirname, 'basic', 'test.md')}`,
-	);
+	const { stderr } = await exec(`node ${resolve(__dirname, '..', 'cli.js')} ${resolve(__dirname, 'basic', 'test.md')}`);
 
-	t.is(stdout, '');
 	t.is(stderr, '');
 });
