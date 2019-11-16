0669992c26f337210d97244927ce86777b026937
simonhaenisch <simonhaenisch@users.noreply.github.com>
2019-11-16 22:29:03 +0100

    style: prettier after merge

--- lib/get-marked-with-highlighter.ts
+++ lib/get-marked-with-highlighter.ts
@@ -1,22 +1,20 @@
-import marked, { MarkedOptions } from "marked";
-import { getLanguage, highlight } from "highlight.js";
+import marked, { MarkedOptions } from 'marked';
+import { getLanguage, highlight } from 'highlight.js';
 
 export const getMarked = (options: MarkedOptions) => {
-  const renderer = options.renderer || new marked.Renderer();
+	const renderer = options.renderer || new marked.Renderer();
 
-  // only add if the renderer has no custom `code` property yet
-  if (!Object.prototype.hasOwnProperty.call(renderer, "code")) {
-    renderer.code = (code, language) => {
-      // if the given language is not available in highlight.js, fall back to plaintext
-      language = (getLanguage(language) && language) || "plaintext";
+	// only add if the renderer has no custom `code` property yet
+	if (!Object.prototype.hasOwnProperty.call(renderer, 'code')) {
+		renderer.code = (code, language) => {
+			// if the given language is not available in highlight.js, fall back to plaintext
+			language = (getLanguage(language) && language) || 'plaintext';
 
-      return `<pre><code class="hljs ${language}">${
-        highlight(language, code).value
-      }</code></pre>`;
-    };
-  }
+			return `<pre><code class="hljs ${language}">${highlight(language, code).value}</code></pre>`;
+		};
+	}
 
-  marked.setOptions({ ...options, renderer });
+	marked.setOptions({ ...options, renderer });
 
-  return marked;
+	return marked;
 };
