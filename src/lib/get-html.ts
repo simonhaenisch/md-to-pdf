d790b2ba9ddaf37f4d915f3877c8cbec2d20540d
Lachlan Newman <lachnewman007@gmail.com>
2022-07-15 17:42:46 +0800

    feat: allow passing custom extensions to marked (#127)

--- src/lib/get-html.ts
+++ src/lib/get-html.ts
@@ -8,7 +8,7 @@ export const getHtml = (md: string, config: Config) => `<!DOCTYPE html>
 <html>
 	<head><title>${config.document_title}</title><meta charset="utf-8"></head>
 	<body class="${config.body_class.join(' ')}">
-		${getMarked(config.marked_options)(md)}
+		${getMarked(config.marked_options, config.marked_extensions)(md)}
 	</body>
 </html>
 `;
